9c05b14d6a96c96a7e5ad47505aeba60f7892e9f
Guilherme Michel <139629718+Gui0r@users.noreply.github.com>
2024-08-23 00:34:08 +0000

    feito varias funções

---
+++ atividade.py
@@ -0,0 +1,64 @@
+class Animal():
+    def __init__(self, animal_especie,nome, idade, barulho, movimento, alimentacao, habitat, vizinhos, horas_alimentacao):
+        self.animal_especie = animal_especie
+        self.nome = nome
+        self.idade = idade
+        self.barulho = barulho
+        self.movimento = movimento
+        self.alimentacao = alimentacao
+        self.habitat = habitat
+        self.vizinhos = vizinhos[:2]
+        self.horas_alimentacao = horas_alimentacao
+
+def barulho(self):
+    return f"O animal {self.nome} está {self.barulho_} ondas sonoras"
+
+def movimento(self)
+    if movimento == true:
+        print(f"O animal {self.nome} esta {self.movimento} !")
+    else
+        return f"O animal {self.nome} não esta {self.movimento} !"
+
+def alimentacao(self):
+    if alimentacao == true:
+        print(f"O animal {self.nome} está se alimentando !")
+    else 
+        return f"O animal {self.nome} não esta se alimentando !"
+
+def __str__(self):
+    return f"nome: {self.nome} idade: {self.idade} barulho: {self.barulho} movimento: {self.movimento} alimentação: {self.alimentacao} habitat: {self.habitat} vizinhos: {self.vizinhos} horas alimentação: {self.horas_alimentacao}"
+
+class Mamifero(self):
+    def __init__(self, animal_especie, nome, idade, barulho, movimento, alimentacao, habitat, vizinhos, horas_alimentacao)
+    super().__init__(animal_especie, nome, idade, barulho, movimento, alimentacao, habitat, vizinhos, horas_alimentacao)
+
+class Ave(self):
+    def __init__(self, animal_especie, nome, idade, barulho, movimento, alimentacao, habitat, vizinhos, horas_alimentacao)
+    super().__init__(animal_especie, nome, idade, barulho, movimento, alimentacao, habitat, vizinhos, horas_alimentacao)
+
+class Reptil(self):
+    def __init__(self, animal_especie, nome, idade, barulho, movimento, alimentacao, habitat, vizinhos, horas_alimentacao)
+    super().__init__(animal_especie, nome, idade, barulho, movimento, alimentacao, habitat, vizinhos, horas_alimentacao)
+
+zoologico = []
+
+def cadastrar_animais():
+    animal_especie = input("Especie do animal: ")
+    nome = input("Nome do animal: ")
+    idade = int(input("Idade: "))
+    barulho = input("Som caracteristico: ")
+    movimento = input("Movimento caracteristico: ")
+    alimentacao = input("Dieta caracteristica: ")
+    habitat = input("Habitat caracteristico: ")
+    vizinhos = input("Vizinhos (separe por virgula. No max 2): ").split(',')[:2]
+    horas_alimentacao = input("Hora que o bixo se alimenta (HH:MM): ")
+    return
+    zoologico.append(Animal)
+    print(f"Animal {nome} foi cadastrado com sucesso!")
+
+def listarAnimal():
+    for nome in zoologico:
+        print(nome)
+
+lagarto = Reptil("roberto", 8, "Rugido", "Voar", "Pesadelos", "Rio dos sinos", [lindomar, lindamer])
+        
